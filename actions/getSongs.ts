354770e0a1d7125df5dc2b5a7b0894d162b2fb79
jaxo <jaroba0gmail.com>
2023-08-14 07:28:25 +0200

    liked songs

--- actions/getSongs.ts
+++ actions/getSongs.ts
@@ -1,19 +1,22 @@
-import {Song} from "@/types"
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import {cookies} from "next/headers"
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-const getSongs= async (): Promise<Song[]> => {
-    const supabase = createServerComponentClient({
-        cookies: cookies
-    })
+const getSongs = async (): Promise<Song[]> => {
+  const supabase = createServerComponentClient({
+    cookies: cookies,
+  });
 
-    const {data , error} = await supabase.from("songs").select("*").order("created_at", {ascending: false})
+  const { data, error } = await supabase
+    .from("songs")
+    .select("*")
+    .order("created_at", { ascending: false });
 
-    if (error) {
-        console.log(error)
-    }
+  if (error) {
+    console.log(error);
+  }
 
-    return (data as any) || []
-}
+  return (data as any) || [];
+};
 
-export default getSongs
+export default getSongs;
--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,31 +1,34 @@
-"use client"
+"use client";
 
-import qs from "query-string"
+import qs from "query-string";
 
-import useDebounce from "@/hooks/useDebounce"
-import { useRouter } from "next/navigation"
-import { useState,useEffect } from "react"
-import Input from "./Input"
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import Input from "./Input";
 
 const SearchInput = () => {
-    const router = useRouter()
-    const [value,setValue] = useState<string>("")
-    const debouncedValue = useDebounce<string>(value,500)
-    useEffect(() => {
-      const query = {
-        title: debouncedValue,
-      }
-      const url = qs.stringifyUrl({
-        url : "/search",
-        query : query
-      })
-      router.push(url)
-    }, [debouncedValue,value,router])
-    
-  return (
-    <Input placeholder="What do you want to listen to?" value={value} onChange={(e) => setValue(e.target.value)}
-    />
-  )
-}
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
+  useEffect(() => {
+    const query = {
+      title: debouncedValue,
+    };
+    const url = qs.stringifyUrl({
+      url: "/search",
+      query: query,
+    });
+    router.push(url);
+  }, [debouncedValue, value, router]);
 
-export default SearchInput
+  return (
+    <Input
+      placeholder="What do you want to listen to?"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+};
+
+export default SearchInput;
