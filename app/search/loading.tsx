e9cb78f2b9a4e0beed11d6d9218d8d1d93d3b781
jaxo <jaroba0gmail.com>
2023-08-16 17:23:59 +0200

    stripe

--- app/search/loading.tsx
+++ app/search/loading.tsx
@@ -7,7 +7,7 @@ import { BounceLoader } from "react-spinners"
 const loading = () => {
   return (
     <Box className="h-full flex items-center justify-center">
-            <BounceLoader color="22c55e" size={40}/>
+            <BounceLoader color="#22c55e" size={40}/>
     </Box>
   )
 }
